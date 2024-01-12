417e0b2d41be2a31dca22927fec405cf92143ef5
Umer Khokhar <umerkhokhar1001@gmail.com>
2024-01-12 22:48:01 +0500

    play feature added

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,6 @@
-export const cardContainer = document.getElementById('cardContainer')
+const cardContainer = document.getElementById('cardContainer');
+const songList = document.getElementById('ul');
+const playAudio = new Audio();
 // console.log(cardContainer)
 
 function loadData(folder) {
@@ -19,6 +21,34 @@ function cardFetch(mydata, folder) {
   <p>${mydata.description}</p>
   `
   cardContainer.appendChild(card);
+
+  card.addEventListener('click', e => {
+    songList.innerHTML = "";
+    for (let index = 0; index < mydata.songs.length; index++) {
+      const li = document.createElement('li');
+      let songName = document.createElement('h3');
+      let authorName = document.createElement('p');
+      songName.innerHTML = `${mydata.songs[index].title}`
+      authorName.innerHTML = `${mydata.songs[index].author}`
+      console.log(songName)
+      li.innerHTML = `
+      <div>
+        ${songName.outerHTML}
+        ${authorName.outerHTML}
+      </div>
+      `
+      
+      li.addEventListener('click', e => {
+        let audioUrl = `/songs/${folder}/${mydata.songs[index].song}`;
+        console.log(audioUrl)
+        playAudio.src = audioUrl;
+        playAudio.play()
+      })
+      
+      songList.appendChild(li)
+      
+    }
+  })
 }
 
 export {loadData, cardFetch}
